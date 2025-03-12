44f19939a9a40437fb4f16f546e8383194f3cb36
GarciaAcevedo668 <conjuri2023@gmail.com>
2025-03-12 17:41:26 -0500

    Add files via upload

---
+++ sec1.Promedio_edades.py
@@ -0,0 +1,15 @@
+#1.	Obtener el promedio de edad de tres personas.
+edad1 = 0
+edad2 = 0
+edad3 = 0
+
+edad1 = int(input("Por favor ingrese la edad1 "))
+edad2 = int(input("Por favor ingrese la edad2 "))
+edad3 = int(input("Por favor ingrese la edad3 "))
+
+ 
+promedio = (edad1+edad2+edad3)/3
+
+print(("La edad promedio de las tres edades es", promedio))
+
+
