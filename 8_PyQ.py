df7b68fbef5e9efd4febebc207944315033e066d
GarciaAcevedo668 <conjuri2023@gmail.com>
2025-03-12 20:31:09 -0500

    Delete 8_PyQ.py

--- 8_PyQ.py
+++
@@ -1,9 +0,0 @@
-p=0
-q=0
-
-resultado = p**3 + q**4 - 2 * p**2
-
-if resultado > 680:
-  print("P y Q satisfacen la expresión. Los valores son: P = " + str(p) + " y Q = " + str(q))
-else:
-  print("P y Q no satisfacen la expresión.")
